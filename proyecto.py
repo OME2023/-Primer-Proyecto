8fe50e02250fa2b22591cc498d069afa2a6568d2
Oscar Falon <oscar.falon@gmail.com>
2023-02-22 09:39:26 -0300

    agregue proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+print("Primer proyecto de prueba en GitHub")
+print("***********************************" "\n")
+
+
+valor = int(input("Ingrese 1 para saludar o 2 para salir: "))
+
+if valor == 1:
+    print("Hola Mundo")
+else:
+    print("Adios")
